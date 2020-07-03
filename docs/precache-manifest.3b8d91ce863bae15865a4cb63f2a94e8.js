a952cb41c5af4e5c4800e10f224be071dcf42ddd
Natalia <nmeneses8@gmail.com>
2020-07-03 16:19:34 -0500

    docs agregado

---
+++ docs/precache-manifest.3b8d91ce863bae15865a4cb63f2a94e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0ca53a8a456bcc2ba2233c2b4a2bd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c0ef716c245f646d631",
+    "url": "/static/css/main.2a99b92e.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c0ef716c245f646d631",
+    "url": "/static/js/main.b994f70a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
